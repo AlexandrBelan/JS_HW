869b6f29035dd733b58c045c3a69939c80807a7a
AlexandrBelan <lirix26@gmail.com>
2024-05-22 01:35:52 +0300

    third task comlete

--- JS_lesson3/Task3.js
+++ JS_lesson3/Task3.js
@@ -1,2 +1,9 @@
 // Task3**. Напишите программу, которая принимает целое положительное число n (одно любое число от 1 до 9), и выводит сумму равную 
 // n + nn + nnn, где n не перемножаются, а конкатенируются
+const N = 5;
+let concatenation = N.toString();
+firstConcatenation = concatenation + concatenation;
+secondConcatenation = firstConcatenation + N.toString();
+let result = N + Number(firstConcatenation) + Number(secondConcatenation);
+console.log(result);
+
