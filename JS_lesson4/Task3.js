e299104336fe63c3bf7c6c6a45cd441353783897
AlexandrBelan <lirix26@gmail.com>
2024-05-26 21:03:52 +0300

    homework 4 all tasks

---
+++ JS_lesson4/Task3.js
@@ -0,0 +1,29 @@
+// Преобразовать Task 2* таким образом, чтобы age принимался через prompt в привязанной вёрстке, а результат выводился в alert
+const age = Number(prompt(`Введи возраст`));
+console.log(age, typeof age)
+const age_2 = 18;
+const age_3 = 60;
+switch (true) {
+    case isNaN(age): {
+         alert('Parameter is not a number!');
+         throw new Error('Parameter is not a number!');   
+         break;
+         
+    } 
+    case age < age_2: {
+        alert(`You dont have access cause your age is ${age} Its less then`);
+        break;
+    }
+    case age >= age_2 && age < age_3: {
+        alert(`Welcome!`);
+        break;
+    }
+    case age > age_3: { 
+        alert(`Keep calm and look Culture channel`);
+        break;
+    }
+    default: {
+        alert("Technical work");
+        break; 
+    }  
+}
