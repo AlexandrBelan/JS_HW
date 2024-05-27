ef970c9c56c9f514d571f610ea73399a4defa34a
AlexandrBelan <lirix26@gmail.com>
2024-05-27 10:56:58 +0300

    homework 4 error correction

--- JS_lesson4/Task2.js
+++ JS_lesson4/Task2.js
@@ -4,23 +4,22 @@
 //   Проверить работу кода на следующих данных 17, 18, 61, "2", "aaa"
 // 3. Преобразовать Task 2 - 1 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
 //   преобразовываясь в number
-const age = "54";
+const age = "12";
 const age_2 = 18;
 const age_3 = 60;
+    if (isNaN(age)){
+        throw new Error('Parameter is not a number!');
+    }
     switch (true) {
-        case Number.isNaN(Number(age)): {
-            throw new Error('Parameter is not a number!');
-            break;
-        } 
-        case Number(age) < age_2: {
+        case age < age_2: {
             console.log(`You dont have access cause your age is ${age} Its less then`);
             break;
         }
-        case Number(age) >= age_2 && Number(age) < age_3: {
+        case age >= age_2 && age < age_3: {
             console.log(`Welcome  !`);
             break;
         }
-        case Number(age) > age_3: { 
+        case age > age_3: { 
             console.log(`Keep calm and look Culture channel`);
             break;
         }
--- JS_lesson4/Task3.js
+++ JS_lesson4/Task3.js
@@ -1,15 +1,12 @@
 // Преобразовать Task 2* таким образом, чтобы age принимался через prompt в привязанной вёрстке, а результат выводился в alert
-const age = Number(prompt(`Введи возраст`));
-console.log(age, typeof age)
+const age = prompt(`Введи возраст`);
 const age_2 = 18;
 const age_3 = 60;
+if (isNaN(age)) {
+    alert('Parameter is not a number!');
+    throw new Error('Parameter is not a number!'); 
+}
 switch (true) {
-    case isNaN(age): {
-         alert('Parameter is not a number!');
-         throw new Error('Parameter is not a number!');   
-         break;
-         
-    } 
     case age < age_2: {
         alert(`You dont have access cause your age is ${age} Its less then`);
         break;
